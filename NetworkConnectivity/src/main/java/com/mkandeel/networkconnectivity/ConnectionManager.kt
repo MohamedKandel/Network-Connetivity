afa582d40bf243d5e29d2f9677979f21a4c04cb1
MohamedKandel <mohamed.hossam29699@gmail.com>
2024-08-19 12:59:05 +0300

    adding isOnline function to check connection when application starts

--- NetworkConnectivity/src/main/java/com/mkandeel/networkconnectivity/ConnectionManager.kt
+++ NetworkConnectivity/src/main/java/com/mkandeel/networkconnectivity/ConnectionManager.kt
@@ -3,6 +3,7 @@ package com.mkandeel.networkconnectivity
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.Network
+import android.net.NetworkCapabilities
 import android.util.Log
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
@@ -70,6 +71,28 @@ class ConnectionManager(private val context: Context) {
             }
         }
 
+    }
+
+    fun isOnline(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (connectivityManager != null) {
+            val capabilities =
+                connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+            if (capabilities != null) {
+                if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                    Log.i("Internet", "NetworkCapabilities.TRANSPORT_CELLULAR")
+                    return true
+                } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                    Log.i("Internet", "NetworkCapabilities.TRANSPORT_WIFI")
+                    return true
+                } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
+                    Log.i("Internet", "NetworkCapabilities.TRANSPORT_ETHERNET")
+                    return true
+                }
+            }
+        }
+        return false
     }
 
     fun listenConnection(owner: LifecycleOwner, listener: OnConnectionChangedListener) {
